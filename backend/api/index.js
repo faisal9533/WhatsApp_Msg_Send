c3e2b77e35cd0ef5613a86daa7439d35d7f5026b
faisal9533 <135017240+faisal9533@users.noreply.github.com>
2026-04-05 11:21:00 +0530

    check

--- backend/api/index.js
+++ backend/api/index.js
@@ -20,6 +20,7 @@ app.use(express.json());
 const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) return;
   try {
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected ✅");
   } catch (error) {
